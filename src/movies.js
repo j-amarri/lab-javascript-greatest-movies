7d9024399257868d194de3375b99b6e039214b5f
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-07-04 15:44:18 +0100

    Iteration 6 - few leftovers from previous iterations

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,84 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+/*
+function getAllDirectors(moviesArray) {
+  const moviesArrayDirectors = moviesArray.map(function (value) {
+    return value.director;
+  });
+  return moviesArrayDirectors;
+}
+*/
+
+function getAllDirectors(moviesArray) {
+  const moviesArrayDirectors = moviesArray.map(value => value.director);
+  return moviesArrayDirectors;
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(moviesArray) {
+  const moviesStevenSpielberg = moviesArray.filter(
+    value =>
+      value.genre.includes('Drama') && value.director === 'Steven Spielberg'
+  );
+  return moviesStevenSpielberg.length;
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+/* function ratesAverage(moviesArray) {
+  let moviesRatesAverage = moviesArray.reduce(
+    (accumulator, currentValue) =>
+      (accumulator + currentValue.rate) / moviesArray.length
+  );
+  return Math.round(moviesRatesAverage * 100) / 100;
+} */
+
+function ratesAverage(moviesArray) {
+  let initialValue = 0;
+  let moviesRatesAverage = moviesArray.reduce(function (
+    accumulator,
+    currentValue
+  ) {
+    return accumulator + currentValue.rate / moviesArray.length;
+  },
+  initialValue);
+  return Math.round(moviesRatesAverage * 100) / 100;
+}
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(moviesArray) {
+  // create a filtered array with drama movies only
+  const dramaMovies = moviesArray.filter(value =>
+    value.genre.includes('Drama')
+  );
+  // calculate average rate of drama movies
+  // return this value as outcome of the overall function
+  return ratesAverage(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(moviesArray) {
+  let yearlyOrderedMovies = moviesArray.sort((a, b) => {
+    if (a.year > b.year) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  console.log(yearlyOrderedMovies);
+  return yearlyOrderedMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+// returns an array of all titles ordered alphabetically
+function orderAlphabetically(moviesArray) {
+  let titleOrderedMovies = moviesArray.map(value => value.title).sort();
+  return titleOrderedMovies;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
